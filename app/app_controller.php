ee6e934c863024ca463439818066034e443aa664
Iain Mullan <iain.mullan@gmail.com>
2010-09-30 10:59:03 +0100

    adding admin prefix

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,10 +1,12 @@
 <?php
 class AppController extends Controller {
+
+	var $helpers = array('Html', 'Form', 'Time');
 	
 	function beforeFilter() {
 		parent::beforeFilter();
-		
-		if (isset($this->params['admin'])) {
+
+		if (isset($this->params['prefix']) && $this->params['prefix']=='admin') {
 			$this->layout = 'admin';
 		}
 	}
