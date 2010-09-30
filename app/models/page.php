332be2d7315cf98724a6a92209ca028c4f6c2610
Iain Mullan <iain.mullan@gmail.com>
2010-09-30 11:12:35 +0100

    adding counter cache

--- app/models/page.php
+++ app/models/page.php
@@ -3,33 +3,15 @@ class Page extends AppModel {
 	var $name = 'Page';
 	var $displayField = 'title';
 
-	var $recursive = -1;
-	var $actsAs = array('Containable');
-
-	var $belongsTo = array('Parent'=>array('className'=>'Page', 'foreignKey'=>'parent_id'));
+	var $belongsTo = array('Parent'=>array('className'=>'Page', 'foreignKey'=>'parent_id', 'counterCache'=>'child_count'));
 	var $hasMany = array('Child'=>array('className'=>'Page', 'foreignKey'=>'parent_id'));
 
 	var $validate = array(
 		'title' => array(
 			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				'rule' => array('notempty')
 			),
-		),
-		'published' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+		)
 	);
 }
 ?>
